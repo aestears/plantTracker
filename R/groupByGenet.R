f612b04427cf12ca0e1d02b60cd512d70e3842e2
Alice Stears <alice.e.stears@gmail.com>
2021-04-23 16:26:36 -0600

    added groupByGenet function that Dave made, which aggregates polygons in the same year by their proximity to one another

---
+++ R/groupByGenet.R
@@ -0,0 +1,52 @@
+## This function will assign grouping variables for each feature of an sf object. Grouping is based on buffers of a
+## user-defined distance overlapping. Usage:
+##
+##            sf = your sf object
+##
+##        sf$groupID = groupByGenet(sf, dist, id)
+##
+##  Where dist = the distance you want the buffer to be (put 2.5 to group polygons that are 5cm from each other)
+##  and id = the identity variable for your polygons. Assumed to be
+
+library(igraph)
+library(Matrix)
+library(sf)
+
+
+groupByGenet = function(sf, buffGen){
+  buffDat = sf::st_buffer(sf, dist = buffGen) ## buffers the data for the focal
+  # year by the buff argument
+  overlaps = sf::st_intersects(buffDat,buffDat) ## identifies which polygons
+  # overlap with each other
+  ## I think we need to trim the overlap list to individuals that overlap a certain amount?
+
+  i = rep(1:length(overlaps), lengths(overlaps))
+  j = factor(unlist(overlaps))
+  tab = Matrix::sparseMatrix(i = i, j = as.integer(j), x = TRUE, dimnames = list(NULL, levels(j)))
+
+  connects = Matrix::tcrossprod(tab, boolArith = TRUE)
+  group = igraph::clusters(graph_from_adjacency_matrix(as(connects, "lsCMatrix")))$membership
+
+  tapply(overlaps, group, function(x) sort(unique(unlist(x))))
+  trackIDs = tapply(1:length(overlaps), group, toString)
+
+  groupID = rep(NA, nrow(sf))
+
+  for(i in 1:nrow(sf)){
+    for(j in 1:length(trackIDs)){
+      vec = as.numeric(#str_trim(
+        unlist(strsplit(trackIDs[j], ",")))
+      #)
+      if(i %in% vec){
+        groupID[i] = j
+
+      } else{
+        next
+      }
+    }
+  }
+ return(groupID)
+}
+
+
+

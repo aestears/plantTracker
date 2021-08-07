0a5ea4feb78c39eb297a1b5ffbc90c5b90d32738
Alice Stears <alice.e.stears@gmail.com>
2021-08-07 15:33:24 -0600

    documentation / touchups for drawQuadMap

--- R/drawQuadMap.R
+++ R/drawQuadMap.R
@@ -1,21 +1,65 @@
-#' Create maps of quadrats over time, color-coded either by species or
-#' by trackID
+#' Create maps of a quadrat over time
 #'
-#' @param dat
-#' @param type
-#' @param species
-#' @param site
-#' @param quad
-#' @param year
-#' @param geometry
-#' @param trackID
+#' @description This function creates maps of a quadrat over time, color-coded
+#' by either species or by genet.
 #'
-#' @return
+#' @param dat An sf data.frame in which each row represents a unique polygon
+#' (either a genet or a ramet) in a unique site/quadrat/year combination. It is
+#' recommended that you only input data for one quadrat at a time. A data.frame
+#' returned by \code{\link{trackSpp}} can be put into this function after being
+#' subset by quadrat. 'dat' must have columns that contain a unique
+#' identification for each research site (default name is "Site"), species name
+#' (default name is "Species"), quadrat identifier (default name is "Quad"),
+#' year of data collection (default name is "Year"), a unique identifier for
+#' each genet (default name is 'trackID') and an s.f 'geometry' column that
+#' contains a polygon or multipolygon data type for each individual observation.
+#' @param type A character argument indicating how the plots returned by
+#' [drawQuadMap()] will be color coded. If [type = "bySpecies"], then each
+#' species is drawn with a unique color. If [type = "bytrackID"], then each
+#' trackID is drawn with a unique color. The default value is "bySpecies".
+#' @param addBuffer A logical argument indicating whether the [drawQuadMap()]
+#' will draw a small buffer around each polygon in the returned maps to make the
+#' observations more visible. This is particularly useful for observations that
+#' were originally mapped as points, which are hard to see when plotted in their
+#' original dimensions. The buffer distance is 1/20th of the quadrat width. The
+#' default value is [FALSE].
+#' @param species An optional character string argument. Indicates
+#' the name of the column in 'dat' that contains species name data. It is
+#' unnecessary to include a value for this argument if the column name is
+#' "Species" (default value is 'Species').
+#' @param site An optional character string argument. Indicates
+#' the name of the column in 'dat' that contains site name data. It is
+#' unnecessary to include a value for this argument if the column name is
+#' "Site" (default value is 'Site').
+#' @param quad An optional character string argument. Indicates
+#' the name of the column in 'dat' that contains quadrat name data. It is
+#' unnecessary to include a value for this argument if the column name is
+#' "Quad" (default is 'Quad').
+#' @param year An optional character string argument. Indicates
+#' the name of the column in 'dat' that contains data for year of sampling. It
+#' is unnecessary to include a value for this argument if the column name is
+#' "Year" (default is 'Year').
+#' @param geometry An optional character string argument. Indicates
+#' the name of the column in 'dat' that contains sf geometry data. It is
+#' unnecessary to include a value for this argument if the column name is
+#' "geometry" (default is 'geometry').
+#' @param trackID An optional character string argument. Indicates the name of
+#' the column in 'dat' that contains unique identfiers for each genet. It is
+#' unnecessary to include a value for this argument if the column name is
+#' "trackID" (default is 'trackID')
+#'
+#' @import sf
+#' @return This funtion returns a multipanel plot where each panel shows a map
+#' of the quadrat in a unique year. Panels are arranged in chronological order,
+#' and plots are color-coded either by species or trackID (unique
+#' genet identfier).
 #' @export
 #'
 #' @examples
 
-drawQuadMap <- function (dat, type = "Species",
+drawQuadMap <- function (dat,
+                         type = "bySpecies",
+                         addBuffer = FALSE,
                          species = "Species",
                          site = "Site",
                          quad = "Quad",
@@ -67,17 +111,17 @@ drawQuadMap <- function (dat, type = "Species",
 
   ## check the 'type' argument
   if (is.character(type) == TRUE) {
-    if (type != "Species" &
-        type != "trackID") {
-      stop("'type' must have either the value 'Species' or 'trackID'")
+    if (type != "bySpecies" &
+        type != "bytrackID") {
+      stop("'type' must have either the value 'bySpecies' or 'bytrackID'")
     }
   } else {
   stop("'type' must be a single character argument with either the value
-       'Species' or 'trackID'.")
+       'bySpecies' or 'bytrackID'.")
   }
 
   ## check the 'trackID' argument (only if 'type' is 'trackID')
-  if (type == "trackID") {
+  if (type == "bytrackID") {
     if (is.character(trackID) == "TRUE") { ## make sure trackID is a chracter
       ## make sure that the value for 'trackID' is a column name in 'dat'
       if (sum(names(dat) %in% trackID) != 1) {
@@ -97,7 +141,7 @@ drawQuadMap <- function (dat, type = "Species",
 
   ## does 'dat' contain sf data?
   if (sum(class(dat) %in% "sf") > 0) { ## if there IS sf data
-    if (sum(!st_is(x = dat, type = c("POLYGON", "MULTIPOLYGON"))) != 0) {
+    if (sum(!sf::st_is(x = dat, type = c("POLYGON", "MULTIPOLYGON"))) != 0) {
       ## if the sf data are not polygon or multipolygon ...
       stop("The sf data in 'dat' must be of either class POLYGON
            or MULTIPOLYGON")
@@ -150,11 +194,26 @@ drawQuadMap <- function (dat, type = "Species",
          labeled 'geometry'")
   }
 
+  ## check the 'addBuffer' argument
+  if (is.logical(addBuffer) == FALSE) {
+    stop("The 'addBuffer' must be a logical argument. If 'addBuffer' = TRUE,
+         then a small buffer will be drawn around individuals to make them
+         easier to see. If 'addBuffer' = FALSE (the default), then a buffer will
+         not be drawn.")
+  }
+
+  ## see how many quadrats have data in 'dat'
+  if (length(paste0(unique(dat$Site), "_", unique(dat$Quad))) > 1) {
+    stop("This function expects data from only one quadrat, and it looks like
+         you've included data for multiple. Make sure to subset your 'dat'
+         data.frame by quadrat, and use the 'drawQuadMap()' function
+         independently on each quadrat's data.")
+  }
 
 # work --------------------------------------------------------------------
 
   ## get the boundary box dimensions
-  quadBox <- st_bbox(dat)
+  quadBox <- sf::st_bbox(dat)
 
   ## get the number of years in the dat
   numYears <- length(unique(dat$Year)) + 1
@@ -168,32 +227,88 @@ drawQuadMap <- function (dat, type = "Species",
 
   ## plot the quadrat maps
   ## (for 'Species' method)
-  if (type == "Species") {
-    for (i in sort(unique(dat$Year))) {
-      datTemp <- dat[dat$Year == i, ]
-      plot(
-        datTemp$geometry,
-        col = as.factor(datTemp$Species),
-        main = paste(unique(datTemp$Year)),
-        xlim = c(quadBox$xmin, quadBox$xmax),
-        ylim = c(quadBox$ymin, quadBox$ymax)
-      )
-      lines(
-        x = c(quadBox$xmin, quadBox$xmin),
-        y = c(quadBox$ymin, quadBox$ymax)
-      )
-      lines(
-        x = c(quadBox$xmax, quadBox$xmax),
-        y = c(quadBox$ymin, quadBox$ymax)
-      )
-      lines(
-        x = c(quadBox$xmin, quadBox$xmax),
-        y = c(quadBox$ymin, quadBox$ymin)
-      )
-      lines(
-        x = c(quadBox$xmin, quadBox$xmax),
-        y = c(quadBox$ymax, quadBox$ymax)
-      )
+  if (type == "bySpecies") {
+    ## add a column to dat (that will later be removed) for color plotting
+    ## make a data.frame with unique spp and colour data
+    colorDat <- data.frame(Species = unique(dat$Species),
+                           ## get color codes for full alpha colors
+                           col = hcl.colors(n = length(unique(dat$Species)),
+                                            palette = "viridis"),
+                           ## get color odes for alpha = .5 colors
+                           colBuff = hcl.colors(n = length(unique(dat$Species)),
+                                            palette = "viridis", alpha = .5))
+    ## add to the 'dat' data.frame
+    dat$col <-
+      colorDat$col[match(x = dat$Species, table = unique(dat$Species))]
+    dat$colBuff <-
+      colorDat$colBuff[match(x = dat$Species, table = unique(dat$Species))]
+    if (addBuffer == FALSE) {
+      for (i in sort(unique(dat$Year))) {
+        datTemp <- dat[dat$Year == i, ]
+        plot(
+          datTemp$geometry,
+          col = datTemp$col,
+          main = paste(unique(datTemp$Year)),
+          xlim = c(quadBox$xmin, quadBox$xmax),
+          ylim = c(quadBox$ymin, quadBox$ymax)
+        )
+        lines(
+          x = c(quadBox$xmin, quadBox$xmin),
+          y = c(quadBox$ymin, quadBox$ymax)
+        )
+        lines(
+          x = c(quadBox$xmax, quadBox$xmax),
+          y = c(quadBox$ymin, quadBox$ymax)
+        )
+        lines(
+          x = c(quadBox$xmin, quadBox$xmax),
+          y = c(quadBox$ymin, quadBox$ymin)
+        )
+        lines(
+          x = c(quadBox$xmin, quadBox$xmax),
+          y = c(quadBox$ymax, quadBox$ymax)
+        )
+      }
+    } else if (addBuffer == TRUE) {
+      ## calculate the buffer (based on the units of the data) -- 1/20th of the quadrat width
+      buffDist <- (quadBox$xmax)/20
+      ## buffer the 'dat' data
+      datBuff <- sf::st_buffer(x = dat, dist = buffDist)
+      ## make the plots
+      for (i in sort(unique(dat$Year))) {
+        datTemp <- dat[dat$Year == i, ]
+        datBuffTemp <- datBuff[datBuff$Year == i,]
+        ## plot buffered data
+        plot(datBuffTemp$geometry,
+             col = datBuffTemp$colBuff,
+             main = paste(unique(datBuffTemp$Year)),
+             xlim = c(quadBox$xmin, quadBox$xmax),
+             ylim = c(quadBox$ymin, quadBox$ymax)
+             )
+        plot(
+          datTemp$geometry,
+          col = datTemp$col,
+          xlim = c(quadBox$xmin, quadBox$xmax),
+          ylim = c(quadBox$ymin, quadBox$ymax),
+          add = TRUE
+        )
+        lines(
+          x = c(quadBox$xmin, quadBox$xmin),
+          y = c(quadBox$ymin, quadBox$ymax)
+        )
+        lines(
+          x = c(quadBox$xmax, quadBox$xmax),
+          y = c(quadBox$ymin, quadBox$ymax)
+        )
+        lines(
+          x = c(quadBox$xmin, quadBox$xmax),
+          y = c(quadBox$ymin, quadBox$ymin)
+        )
+        lines(
+          x = c(quadBox$xmin, quadBox$xmax),
+          y = c(quadBox$ymax, quadBox$ymax)
+        )
+      }
     }
     ## make a 'plot' that is a legend
     plot(
@@ -207,38 +322,95 @@ drawQuadMap <- function (dat, type = "Species",
       x = quadBox$xmin,
       y = quadBox$ymax,
       legend = unique(datTemp$Species),
-      fill = as.factor(unique(datTemp$Species)),
+      fill = unique(datTemp$col),
       bty = "n",
       cex = 1,
       x.intersp = .2
     )
-  } else if (type == "trackID") {
-    for (i in sort(unique(dat$Year))) {
-      datTemp <- dat[dat$Year == i, ]
-      plot(
-        datTemp$geometry,
-        col = as.factor(datTemp$trackID),
-        main = paste(unique(datTemp$Year)),
-        xlim = c(quadBox$xmin, quadBox$xmax),
-        ylim = c(quadBox$ymin, quadBox$ymax)
-      )
-      lines(
-        x = c(quadBox$xmin, quadBox$xmin),
-        y = c(quadBox$ymin, quadBox$ymax)
-      )
-      lines(
-        x = c(quadBox$xmax, quadBox$xmax),
-        y = c(quadBox$ymin, quadBox$ymax)
-      )
-      lines(
-        x = c(quadBox$xmin, quadBox$xmax),
-        y = c(quadBox$ymin, quadBox$ymin)
-      )
-      lines(
-        x = c(quadBox$xmin, quadBox$xmax),
-        y = c(quadBox$ymax, quadBox$ymax)
-      )
+  } else if (type == "bytrackID") {
+    ## add a column to dat (that will later be removed) for color plotting
+    ## make a data.frame with unique trackID and colour data
+    colorDat <- data.frame(trackID = unique(dat$trackID),
+                           ## get color codes for full alpha colors
+                           col = hcl.colors(n = length(unique(dat$trackID)),
+                                            palette = "viridis"),
+                           ## get color odes for alpha = .5 colors
+                           colBuff = hcl.colors(n = length(unique(dat$trackID)),
+                                                palette = "viridis", alpha = .5))
+    ## add to the 'dat' data.frame
+    dat$col <-
+      colorDat$col[match(x = dat$trackID, table = unique(dat$trackID))]
+    dat$colBuff <-
+      colorDat$colBuff[match(x = dat$trackID, table = unique(dat$trackID))]
+    ## make the plots
+    if (addBuffer == TRUE) {## calculate the buffer (based on the units of the data) -- 1/20th of the quadrat width
+      buffDist <- (quadBox$xmax)/20
+      ## buffer the 'dat' data
+      datBuff <- sf::st_buffer(x = dat, dist = buffDist)
+      ## make the plots
+      for (i in sort(unique(dat$Year))) {
+        datTemp <- dat[dat$Year == i, ]
+        datBuffTemp <- datBuff[datBuff$Year == i,]
+        plot(
+          datBuffTemp$geometry,
+          col = datBuffTemp$colBuff,
+          main = paste(unique(datTemp$Year)),
+          xlim = c(quadBox$xmin, quadBox$xmax),
+          ylim = c(quadBox$ymin, quadBox$ymax)
+        )
+        plot(
+          datTemp$geometry,
+          col = datTemp$col,
+          xlim = c(quadBox$xmin, quadBox$xmax),
+          ylim = c(quadBox$ymin, quadBox$ymax),
+          add = TRUE
+        )
+        lines(
+          x = c(quadBox$xmin, quadBox$xmin),
+          y = c(quadBox$ymin, quadBox$ymax)
+        )
+        lines(
+          x = c(quadBox$xmax, quadBox$xmax),
+          y = c(quadBox$ymin, quadBox$ymax)
+        )
+        lines(
+          x = c(quadBox$xmin, quadBox$xmax),
+          y = c(quadBox$ymin, quadBox$ymin)
+        )
+        lines(
+          x = c(quadBox$xmin, quadBox$xmax),
+          y = c(quadBox$ymax, quadBox$ymax)
+        )
+      }
+    } else if (addBuffer == FALSE) {
+      for (i in sort(unique(dat$Year))) {
+        datTemp <- dat[dat$Year == i, ]
+        plot(
+          datTemp$geometry,
+          col = unique(datTemp$col),
+          main = paste(unique(datTemp$Year)),
+          xlim = c(quadBox$xmin, quadBox$xmax),
+          ylim = c(quadBox$ymin, quadBox$ymax)
+        )
+        lines(
+          x = c(quadBox$xmin, quadBox$xmin),
+          y = c(quadBox$ymin, quadBox$ymax)
+        )
+        lines(
+          x = c(quadBox$xmax, quadBox$xmax),
+          y = c(quadBox$ymin, quadBox$ymax)
+        )
+        lines(
+          x = c(quadBox$xmin, quadBox$xmax),
+          y = c(quadBox$ymin, quadBox$ymin)
+        )
+        lines(
+          x = c(quadBox$xmin, quadBox$xmax),
+          y = c(quadBox$ymax, quadBox$ymax)
+        )
+      }
     }
+
     ## make a 'plot' that is a legend
     plot(
       x = NULL,
@@ -251,7 +423,7 @@ drawQuadMap <- function (dat, type = "Species",
       x = quadBox$xmin,
       y = quadBox$ymax,
       legend = unique(datTemp$trackID),
-      fill = as.factor(unique(datTemp$trackID)),
+      fill = unique(datTemp$col),
       bty = "n",
       cex = 1,
       x.intersp = .2

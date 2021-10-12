46ab5215b690a74e71f67c344defdc2ca78f1375
Alice Stears <alice.e.stears@gmail.com>
2021-10-12 14:16:55 -0600

    updating documentation

--- R/getLambda.R
+++ R/getLambda.R
@@ -322,8 +322,8 @@ getLambda <- function(dat,
 
 # testing -----------------------------------------------------------------
 #
-dat <- grasslandData[grasslandData$Site == "CO" & grasslandData$Year %in% c(1998:2002),]
-names(dat)[1] <- "speciesName"
-inv <- grasslandInventory[unique(dat$Quad)]
-outDat <- trackSpp(dat = dat, inv = inv, dorm = 1, buff = .05,buffGenet = 0.005,clonal = data.frame("Species" = unique(dat$speciesName),"clonal" = c(TRUE,FALSE)), species = "speciesName",aggregateByGenet = TRUE)
-getLambda(dat = outDat, inv = inv, species = "speciesName", method = "count")
+# dat <- grasslandData[grasslandData$Site == "CO" & grasslandData$Year %in% c(1998:2002),]
+# names(dat)[1] <- "speciesName"
+# inv <- grasslandInventory[unique(dat$Quad)]
+# outDat <- trackSpp(dat = dat, inv = inv, dorm = 1, buff = .05,buffGenet = 0.005,clonal = data.frame("Species" = unique(dat$speciesName),"clonal" = c(TRUE,FALSE)), species = "speciesName",aggregateByGenet = TRUE)
+# getLambda(dat = outDat, inv = inv, species = "speciesName", method = "count")

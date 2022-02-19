8e65369503fb7a9f5ec518b1121814cad9029580
Alice Stears <alice.e.stears@gmail.com>
2022-02-19 16:35:14 -0700

    fixing the messed-up test for getNeighbors--was messed up by the updates to that function

--- tests/testthat/test-getNeighbors.R
+++ tests/testthat/test-getNeighbors.R
@@ -12,13 +12,25 @@ outDat <- assign(
   clonal = TRUE
 )
 
-locals <- getNeighbors(dat = outDat, buff = .1, method = "area",
+# using the 'area' method
+locals_1 <- getNeighbors(dat = outDat, buff = .1, method = "area",
                        compType = "oneSpp")
 
+# using the 'count' method
+locals_2 <- getNeighbors(dat = outDat, buff = .1, method = "count",
+                         compType = "oneSpp")
+
+# tests:
 test_that("output is the same number of rows as the input", {
- expect_equal(object = nrow(locals), expected = nrow(dat))
+ expect_equal(object = nrow(locals_1), expected = nrow(dat))
+})
+test_that("output is the same number of rows as the input", {
+  expect_equal(object = nrow(locals_2), expected = nrow(dat))
 })
 
-test_that("output has only one more column than the input", {
-  expect_equal(object = ncol(locals), expected = ncol(outDat)+1)
+test_that("output has only one more column than the input (for method = 'count')", {
+  expect_equal(object = ncol(locals_2), expected = ncol(outDat)+1)
+})
+test_that("output has only two more columns than the input (for method = 'area')", {
+  expect_equal(object = ncol(locals_1), expected = ncol(outDat)+2)
 })
